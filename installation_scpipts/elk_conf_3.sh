e1a56d8ae8cae919503de923e48ba9abdd3a7341
Jay7 <giannisotiro@gmail.com>
2015-09-07 02:39:30 +0300

    adding kibana install configurations

--- installation_scpipts/elk_conf_3.sh
+++ installation_scpipts/elk_conf_3.sh
@@ -4,6 +4,9 @@
 
 ES_HOME=$HOME/elasticsearch-1.7.1
 ES_CONFIG=$ES_HOME/config/elasticsearch.yml #/etc/elasticsearch/elasticsearch.yml
+KBN_HOME=$HOME/kibana-4.1.1-linux-x64
+KBN_CONFIG=$KIBANA_HOME/config/kibana.yml
+IP_ETH0=`ifconfig eth0 | grep 'inet addr'| awk '{print $2;}'|awk '{split($0,a,":");print a[2]}'`
 
  #to restrict outside access to your Elasticsearch instance (port 9200), so outsiders can't read your data or shutdown your Elasticsearch cluster through the HTTP API
  #also we give a name to our cluster and our node which is used to discover and auto-join other nodes
@@ -13,7 +16,7 @@ ex $ES_CONFIG <<EOEX1
  :%s/#cluster.name:\zs.*//
  :%s/#cluster.name:/cluster.name: elk-cluster/g
  :%s/#node.name:\zs.*//
- :%s/#node.name:/node.name: "elk-node"/g
+ :%s/#node.name:/node.name: \${prompt.text}
  :x
 EOEX1
 
@@ -25,6 +28,49 @@ sudo update-rc.d elasticsearch defaults 95 10
 comment1
 
 <<comment2
+ #this setting makes it so Kibana will only be accessible to the localhost. This is fine because we will use an Nginx reverse proxy to allow external access.
+ex KBN_CONFIG<<EOEX3
+ :%s/host: "0.0.0.0"/host: "localhost"/g
+ :x
+EOEX3
+ 
+ ##install and set nginx
+ #Because we configured Kibana to listen on localhost, we must set up a reverse proxy to allow external access to it. We will use Nginx for this purpose
+sudo apt-get install nginx apache2-utils
+sudo htpasswd -bc kibanaadmin kibana > auth/htpasswd
+touch /etc/nginx/conf.d/kibana.conf 
+sudo cat <<EOF > /etc/nginx/conf.d/kibana.conf
+ server {
+  listen 80;
+  server_name $IP_ETH0;
+  auth_basic "Restricted Access";
+  auth_basic_user_file /etc/nginx/htpasswd.users;
+
+  location / {
+   proxy_pass http://localhost:5601;
+   proxy_http_version 1.1;
+   proxy_set_header Upgrade \$http_upgrade;
+   proxy_set_header Connection 'upgrade';
+   proxy_set_header Host \$host;
+   proxy_cache_bypass \$http_upgrade;  
+   }
+  }
+EOF
+sudo service nginx restart
+comment2
+
+<<comment3
+ #copy the Kibana files to a more appropriate location
+sudo mkdir -p /opt/kibana
+sudo cp -R ~/kibana-4*/* /opt/kibana/
+ #we need to have Kibana 4 start up when the machine boots so we need to have it run as a service
+sudo wget --output-document="/etc/init.d/kibana4" https://raw.githubusercontent.com/akabdog/scripts/master/kibana4_init
+sudo chmod +x /etc/init.d/kibana4
+
+#sudo service kibana4 start
+comment3
+
+<<comment4
  #try to lock the process address space into RAM, preventing any Elasticsearch memory from being swapped out
 ex $ES_CONFIG <<EOEX
  :%s/#bootstrap.mlockall: true/bootstrap.mlockall: true/g
@@ -35,6 +81,6 @@ cd $ES_HOME
 sudo su
 ulimit -l unlimited
 ./bin/elasticsearch -d
-comment2
+comment4
 
 
--- installation_scpipts/install_elk_2.sh
+++ installation_scpipts/install_elk_2.sh
@@ -5,15 +5,15 @@
  ##install java
  #it may requires to type the root password
  #add the Oracle Java PPA to apt
+cd $HOME
 sudo add-apt-repository -y ppa:webupd8team/java
  #update your apt package database
 sudo apt-get update
  #Install the latest stable version of Oracle Java and accept the license agreement that pops up
 sudo apt-get -y install oracle-java8-installer
 
- ##install Elasticsearch
+ ##install Elasticsearch 1.7.1
  #import the Elasticsearch public GPG key into apt  
-
  <<comment1
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
  #create the Elasticsearch source list 
@@ -21,15 +21,14 @@ echo "deb http://packages.elastic.co/elasticsearch/1.7/debian stable main" | sud
 sudo apt-get update && sudo apt-get install elasticsearch
 comment1
 
- ##configure Elasticsearch to automatically start during bootup (in a SysV init distribution)
-sudo update-rc.d elasticsearch defaults 95 10
-#else diable it or remove it
-#sudo update-rc.d elasticsearch disable #or "remove"
- 
  #or download the tar file and then unzip it
 cd $HOME
 wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.1.tar.gz
 tar xzf elasticsearch-1.7.1.tar.gz
 
 
+ ##install kibana 4.1.1
+cd $HOME
+wget https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz
+tar xzf kibana-4.1.1-linux-x64.tar.gz
 
