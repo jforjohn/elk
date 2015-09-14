8478888f09430085816a4083ddfd76a71ea22ce9
Jay7 <giannisotiro@gmail.com>
2015-09-14 20:40:17 +0300

    generate and send traffic

---
+++ traffic/p2pconnection.sh
@@ -0,0 +1,25 @@
+# Learn the container process IDs
+# and create their namespace entries
+sudo mkdir -p /var/run/netns
+OVS_PID=`docker inspect -f '{{.State.Pid}}' ovs`
+TCPR_PID=`docker inspect -f '{{.State.Pid}}' tcpreplay`
+
+sudo ln -s /proc/$OVS_PID/ns/net /var/run/netns/$OVS_PID
+sudo ln -s /proc/$TCPR_PID/ns/net /var/run/netns/$TCPR_PID
+
+PEER_A="veth_ovs"
+PEER_B="veth_tcpr"
+IP_A=10.1.1.1/32
+IP_B=10.1.1.2/32
+# Create the "peer" interfaces and hand them out
+sudo ip link add $PEER_B type veth peer name $PEER_A
+
+sudo ip link set $PEER_A netns $OVS_PID
+sudo ip netns exec $OVS_PID ip addr add $IP_A dev $PEER_A
+sudo ip netns exec $OVS_PID ip link set $PEER_A up
+sudo ip netns exec $OVS_PID ip route add $IP_B dev $PEER_A
+
+sudo ip link set $PEER_B netns $TCPR_PID
+sudo ip netns exec $TCPR_PID ip addr add $IP_B dev $PEER_B
+sudo ip netns exec $TCPR_PID ip link set $PEER_B up
+sudo ip netns exec $TCPR_PID ip route add $IP_A dev $PEER_B
---
+++ traffic/traffic.sh
@@ -0,0 +1,17 @@
+sudo docker rm -f ovs tcpreplay
+
+sudo rm -rf sudo /var/run/netns/
+
+docker run -itd --name ovs --cap-add NET_ADMIN --cap-add SYS_MODULE -v /lib/modules:/lib/modules --link logstash socketplane/openvswitch
+
+docker run -itd --name tcpreplay --net=none -v ~/elk/traffic/tcpreplay/pcapfiles:/root/ jaysot/tcpreplay
+
+sh ~/elk/traffic/p2pconnection.sh
+
+docker exec ovs modprobe openvswitch  
+docker exec ovs supervisorctl restart ovs-vswitchd
+docker exec ovs ovs-vsctl add-br br0 
+docker exec ovs ovs-vsctl add-port br0 veth_ovs 
+docker exec ovs ovs-vsctl -- --id=@sflow create sflow agent=eth0 target=\"172.17.0.82:6543\" header=128 sampling=64 polling=10 -- set bridge br0 sflow=@sflow
+
+#docker exec tcpreplay tcpreplay --intf1=veth_tcpr /root/smia.pcap
