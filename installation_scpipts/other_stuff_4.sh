bbf29e811177641402c4f6825c061e60ecd80ccd
Jay7 <giannisotiro@gmail.com>
2015-09-21 22:25:27 +0300

    other_stuff_4.sh added

---
+++ installation_scpipts/other_stuff_4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ #install pip
+sudo apt-get install python-pip
+
+ #install virtualenv, which  is a tool to create isolated Python environments
+#wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-13.1.2.tar.gz#md5=b989598f068d64b32dead530eb25589a
+sudo apt-get install python-virtualenv
+#mkdir flask-api
+#cd flask-api
+virtualenv flask 
+flask/bin/pip install flask 
+
