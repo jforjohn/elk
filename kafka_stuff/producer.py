944b11b2859f4c8d4db74723cf6ca0d20f31eb30
Jay7 <giannisotiro@gmail.com>
2015-11-25 14:48:10 +0200

    add an exception mainly for LeaderAvailableError

--- kafka_stuff/producer.py
+++ kafka_stuff/producer.py
@@ -39,17 +39,17 @@ if __name__ == "__main__":
     #conn, addr = s.accept()
     #print 'Connected with ' + addr[0] + ':' + str(addr[1])        
     #brokerList = ["elk1:9092", "elk2:9092"]
-    brokerList = ["192.168.56.101:9092"]
-    #kafka = KafkaClient(brokerList)
-    #producer = SimpleProducer(kafka, async=True, batch_send=True, batch_send_every_n=10, batch_send_every_t=5)
+    brokerList = ["192.168.56.101:9092","192.168.56.102:9092"]
+    kafka = KafkaClient(brokerList)
+    producer = SimpleProducer(kafka, async=True, batch_send=True, batch_send_every_n=10, batch_send_every_t=5)
 
     #sflowToolProc = subprocess.Popen("sflowtool -l -p 6343".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
     #out, err = sflowToolProc.communicate()
     count = 0
-    print 0
+    print "Producer starts..."
 
-    for line in runProcess('sflowtool -l'.split()):
+    for line in runProcess('sh sflowtool-wrapper.sh -l'.split()):
         fields = line.split(",")
         count += 1
         if fields[0] == "FLOW":
@@ -69,14 +69,19 @@ if __name__ == "__main__":
             tcpFlag = fields[16]
             packetSize = fields[17]
             #ipSize = fields[18]
-            sampleRate = fields[19]
+            sampleRate = fields[19][:-1]
             dateTime = int(time.time()*1000000)
             protoName = 0#mapProtocol(protocol)
             srcService = 0#mapPort(srcPort)
             dstService = 0#mapPort(dstPort)
             tcpFlagType = 0#mapTCPflag(tcpFlag)
             message = "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(srcMAC,dstMAC,etherType,srcVlan,dstVlan,srcIP,dstIP,protocol,protoName,srcPort,srcService,dstPort,dstService,tcpFlag,tcpFlagType,packetSize,sampleRate,dateTime,count)
-            print message
+            try:
+                producer.send_messages("netdata", message)
+            except:
+                print "1s sleep"
+                time.sleep(1)
+                producer.send_messages("netdata", message)
             #producer.send_messages("netdata2", message)
-
+            print message
     #s.close()
