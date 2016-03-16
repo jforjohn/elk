c5e91c46a7195698809bf51b47e0f1edce232d11
jforjohn <giannisotiro@gmail.com>
2016-03-16 05:01:27 +0200

    adding es parallel_bulk and fixing the timestamp issue for kibana

--- dev/kafka-0.9.0.1/kafka2es.py
+++ dev/kafka-0.9.0.1/kafka2es.py
@@ -2,16 +2,19 @@
 from kafka import KafkaConsumer
 from kafka.coordinator.assignors.roundrobin import RoundRobinPartitionAssignor
 from elasticsearch import Elasticsearch, helpers
-from translateIP import mapIP
+#from translateIP import mapIP
 import logging
 import signal
 import sys
-import json
+import yaml
+from yaml import load
+from yaml import CLoader as Loader
 import time, datetime
 
 class MessageHandler:
   def __init__(self):
     self.data = dict()
+    self.send_data = list()
 
   def Accept(self, body, message, es):
     try:
@@ -20,85 +23,105 @@ class MessageHandler:
     except Exception, e:
       loggerConsumer.error('Discarding message - failed to append to payload: %s' % e)
 
-    #if len(self.payload) >= 10:
-    topic = message.topic
-    self.PushMessage(topic, es)
+    self.Encapsulate()
+    print len(self.send_data)
+    if len(self.send_data) >= 40:
+      #topic = message.topic
+      self.PushMessage(es)
 
   def EmbedData(self, body):
     sflowSample = dict()
-    
-    #sflowSample['@timestamp']
+    '''
     timestamp = 'T'.join(
                 str(datetime.datetime.now())
                .split())[0:23] + 'Z'
-
+    '''
+    timestamp = int(time.time() * 1000)
+    
+    print timestamp
     fields = body.split(',')
     if fields[0] == "FLOW":
-      #reporterIP = fields[1]
-      #inPort = fields[2]
-      #outPort = fields[3]
-      srcMAC = fields[4]
-      dstMAC = fields[5]
-      etherType = fields[6]
-      srcVlan = fields[7]
-      dstVlan = fields[8]
-      srcIP = fields[9]
-      dstIP = fields[10]
-      protocol = fields[11]
-      #ipTOS = fields[12]
-      #ipTTL = fields[13]
-      srcPort = fields[14]
-      dstPort = fields[15]
-      tcpFlag = fields[16]
-      packetSize = fields[17]
-      #ipSize = fields[18]
-      sampleRate = fields[19]
-      dateTime = int(time.time()*1000000)
-
+      sflow_ReporterIP = fields[1]
+      sflow_inputPort = fields[2]
+      sflow_outputPort = fields[3]
+      sflow_srcMAC = fields[4]
+      sflow_dstMAC = fields[5]
+      sflow_EtherType = fields[6]
+      sflow_srcVlan = fields[7]
+      sflow_dstVlan = fields[8]
+      sflow_srcIP = fields[9]
+      sflow_dstIP = fields[10]
+      sflow_IP_Protocol = fields[11]
+      sflow_IPTOS = fields[12]
+      sflow_IPTTL = fields[13]
+      sflow_srcPort = fields[14]
+      sflow_dstPort = fields[15]
+      sflow_tcpFlags = fields[16]
+      sflow_PacketSize = fields[17]
+      sflow_IPSize = fields[18]
+      sflow_SampleRate = fields[19]
+      try:
+        sflow_counter = fields[20]
+      except:
+        sflow_counter = -1
+      #dateTime = int(time.time()*1000000)
       
-      [srcIPnew, dstIPnew] = map(mapIP, [srcIP,dstIP])
+      #[sflow_NEWsrcIP,sflow_NEWdstIP] = map(mapIP, [sflow_srcIP,sflow_dstIP])
       #srcIPnew = mapIP(srcIP)
       #dstIPnew = mapIP(dstIP)
+      [sflow_NEWsrcIP, sflow_NEWdstIP] = map(self.mapIP, [sflow_srcIP,sflow_dstIP])
+
+      [sflow_inputPort,sflow_outputPort,sflow_srcVlan,sflow_dstVlan,sflow_IP_Protocol,sflow_IPTTL,sflow_srcPort,sflow_dstPort,sflow_PacketSize,sflow_IPSize,sflow_SampleRate,sflow_SampleRate] = map(int, [sflow_inputPort,sflow_outputPort,sflow_srcVlan,sflow_dstVlan,sflow_IP_Protocol,sflow_IPTTL,sflow_srcPort,sflow_dstPort,sflow_PacketSize,sflow_IPSize,sflow_SampleRate,sflow_SampleRate])
 
       sflowSample = {
-      #'reporterIP':reporterIP,
-      #'inPort':inPort,
-      #'outPort':outPort
+      '@message':body,
       '@timestamp':timestamp,
-      'srcMAC':srcMAC,
-      'dstMAC':dstMAC,
-      'etherType':etherType,
-      'srcVlan':srcVlan,
-      'dstVlan':dstVlan,
-      'srcIP':srcIP,
-      'dstIP':dstIP,
-      'protocol':protocol,
-      #'ipTOS':ipTOS,
-      #'ipTTL':ipTTL,
-      'srcPort':srcPort,
-      'dstPort':dstPort,
-      'tcpFlag':tcpFlag,
-      'packetSize':packetSize,
-      #'ipSize':ipSize,
-      'sampleRate':sampleRate,
-      'srcIPnew':srcIPnew,
-      'dstIPnew':dstIPnew
+      '@version':1,
+      'type':'sflow',
+      'SampleType':'FLOW',
+      'sflow_ReporterIP':sflow_ReporterIP,
+      'sflow_inputPort':sflow_inputPort,
+      'sflow_outputPort':sflow_outputPort,
+      'sflow_srcMAC':sflow_srcMAC,
+      'sflow_dstMAC':sflow_dstMAC,
+      'sflow_EtherType':sflow_EtherType,
+      'sflow_srcVlan':sflow_srcVlan,
+      'sflow_dstVlan':sflow_dstVlan,
+      'sflow_srcIP':sflow_srcIP,
+      'sflow_dstIP':sflow_dstIP,
+      'sflow_IP_Protocol':sflow_IP_Protocol,
+      'sflow_IPTOS':sflow_IPTOS,
+      'sflow_IPTTL':sflow_IPTTL,
+      'sflow_srcPort':sflow_srcPort,
+      'sflow_dstPort':sflow_dstPort,
+      'sflow_tcpFlags':sflow_tcpFlags,
+      'sflow_PacketSize':sflow_PacketSize,
+      'sflow_IPSize':sflow_IPSize,
+      'sflow_SampleRate':sflow_SampleRate,
+      'sflow_NEWsrcIP':sflow_NEWsrcIP,
+      'sflow_NEWdstIP':sflow_NEWdstIP
       }
     else:
-      sflowSample = body
+      sflowSample = {'type':body}
 
     return sflowSample
 
+  def mapIP(self,param):
+    try:
+      return test[param]
+    except:
+      return param
+
   def Encapsulate(self):
-    datestr  = time.strftime('%Y-%m-%d')
-    indexstr = '%s-%s' % ('sflow1', datestr)
+    datestr  = time.strftime('%Y.%m.%d')
+    indexstr = '%s-%s' % ('sflow', datestr)
     
-    send_data = [{
+    self.send_data.append({
       '_index' : indexstr,
       '_type': 'sflow',
       '_source': self.data
       
-    }]
+    })
     
     loggerIndex.info('Compiling Elasticsearch payload with  records') #% len(self.payload))
     #header = json.dumps(header)
@@ -108,18 +131,19 @@ class MessageHandler:
     #for record in self.payload:
     #  data += '%s\n%s\n' % (header, json.dumps(record))
 
-    return send_data
+    #return send_data
 
-  def PushMessage(self, topic, es):
-    send_data = self.Encapsulate()
+  def PushMessage(self, es):
     try:
       #r = requests.post('%s/_bulk?' % args.elasticserver, data=data, timeout=args.timeout)
       #helpers.parallel_bulk(es, data, chunk_size=5)
-      for success, info in helpers.parallel_bulk(es, send_data, chunk_size=200):
-        print '\n', info, success
+      for success, info in helpers.parallel_bulk(es, self.send_data, chunk_size=1500):
+        #print '\n', info, success
+        print info, success
         if not success:
           print('A document failed:', info)
       self.data = {}
+      self.send_data = []
       loggerIndex.info('Bulk API request to Elasticsearch returned with code ' )
     except Exception, e:
       loggerIndex.error('Failed to send to Elasticsearch: %s' % e)
@@ -175,7 +199,12 @@ def SetupES():
 if __name__ == '__main__':
   global loggerConsumer
   global loggerIndex
+  global test
   # create logger
+
+  stream = open("jsonIPmap.yaml", 'r')
+  test = yaml.load(stream, Loader=Loader)
+  
   loggerConsumer = logging.getLogger('kafka consumer')
   loggerConsumer.setLevel(logging.DEBUG)
 
